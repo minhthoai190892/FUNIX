4fa69956a520c0cde9158d9ec9523ea0f522090b
ASUS <minhthoai190892@gmail.com>
2022-09-19 12:41:26 +0700

    dev

--- Java/lean_java_12h/src/App.java
+++ Java/lean_java_12h/src/App.java
@@ -1,6 +1,9 @@
-import java.util.Scanner;
+import javax.swing.JOptionPane;
+
+// import java.util.Scanner;
 
 public class App {
+
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
         /*
@@ -27,24 +30,43 @@ public class App {
          * System.out.println("y: " + y1);
          * System.out.println("temp: " + temp);
          */
-        System.out.println("============= Scanner ==================");
-        try (/**
-              * Scanner: người dùng nhập dữ liệu
-              */
-                Scanner scanner = new Scanner(System.in)) {
-            System.out.println("What is your name?: ");
-            // khai báo biến để nhận giá trị (string) người dùng nhập
-            String name = scanner.nextLine();
-            // khai báo biến nhận giá trị "number"
-            System.out.println("How old are you: ");
-            int age = scanner.nextInt();
-            // xoa ky tu "/n"
-            scanner.nextLine();
-            System.out.println("what is your favorite food?: ");
-            // String food = scanner.nextLine();
-            String food = scanner.nextLine();
-            System.out.println("Hello: " + name + " you are " + age + " years old" + "you like food: " + food);
-            // System.out.println("Age: " + age);
-        }
+
+        // System.out.println("============= Scanner ==================");
+
+        // try (/**
+        // * Scanner: người dùng nhập dữ liệu
+        // */
+        // //tạo đối tượng "Scanner" với từ khóa "new"
+        // Scanner scanner = new Scanner(System.in)) {
+        // System.out.println("What is your name?: ");
+        // // khai báo biến để nhận giá trị (string) người dùng nhập
+        // String name = scanner.nextLine();
+        // // khai báo biến nhận giá trị "number"
+        // System.out.println("How old are you: ");
+        // int age = scanner.nextInt();
+        // // xoa ky tu "/n"
+        // scanner.nextLine();
+        // System.out.println("what is your favorite food?: ");
+        // // String food = scanner.nextLine();
+        // String food = scanner.nextLine();
+        // System.out.println("Hello: " + name + " you are " + age + " years old" + "you
+        // like food: " + food);
+        // // System.out.println("Age: " + age);
+        // }
+        /*
+         * System.out.
+         * println("================== Experssion & operands & operators =========================="
+         * );
+         * double friends = 10;
+         * friends = (double)friends /3;
+         * 
+         * System.out.println(friends);
+         */
+
+        System.out.println("================== GUI intro ==========================");
+        String name = JOptionPane.showInputDialog("Enter your name");
+        JOptionPane.showMessageDialog(null, "Hello " + name);
+        JOptionPane.showMessageDialog(null, "sadfasdf", "qweqwe", JOptionPane.ERROR_MESSAGE);
+
     }
 }
