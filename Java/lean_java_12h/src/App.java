67bdb44959df127876e2ef12568b3ea3aef81931
minhthoai <minhthoai190892@gmail.com>
2022-09-21 18:49:12 +0700

    dev

--- Java/lean_java_12h/src/App.java
+++ Java/lean_java_12h/src/App.java
@@ -171,13 +171,17 @@ public class App {
         // }
 
         System.out.println("================== String method ==========================");
-        String name = "Bro";
+        String name = "Bro    ";
         // boolean result = name.equals("asdf");
         // boolean result = name.equalsIgnoreCase("bro");
         // int result = name.length();
         // char result = name.charAt(0);
         // int result = name.indexOf("r");
-        boolean result = name.isEmpty();
+        // boolean result = name.isEmpty();
+        // String result = name.toLowerCase();
+        // String result = name.toUpperCase();
+        // String result = name.trim();
+        String result = name.replace("B", "P");
         System.out.println(result);
 
     }
