18d8cc6d22b06fd4f51148171e43d4e901c5b679
ASUS <minhthoai190892@gmail.com>
2022-10-19 10:30:50 +0700

    a

---
+++ Assignment/Assignment2/src/User.java
@@ -0,0 +1,17 @@
+public class User {
+    private String name;
+    private String customerId;
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getCustomerId() {
+        return customerId;
+    }
+    public void setCustomerId(String customerId) {
+        this.customerId = customerId;
+    }
+    
+}
